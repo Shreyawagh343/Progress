3e55d1703d129312dd3af8dc1434a8c89ddd1252
shreyawagh343 <shreyawagh343@gmail.com>
2025-11-07 19:34:24 +0530

    day-6

--- day-6/express/index.js
+++ day-6/express/index.js
@@ -1,18 +1,20 @@
-
 const express = require("express");
 const app =express();
+
 function sum(n){
-    let a = 0;
-    for( let i=0;i<=n;i++){
-         a+=i;
+    let ans = 0;
+    for( let i=1;i<=n;i++){
+         ans+=i;
     }
+    return ans;
 
 }
 
 app.get("/",function(req,res){
-    const n = req.qurey.n;
+    const n = req.query.n;
     const ans = sum(n);
-    res.send(ans.toString());
+    console.log(ans);
+    res.send(" hi shreya " + ans);
 })
 
 app.listen(3000);
--- day-6/express/indexHos.js
+++ day-6/express/indexHos.js
@@ -1,19 +1,56 @@
-var users = [{
-    name:"shreya",
-    kidneys:[{
-        heathly:"true",
-    }]
-}]
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-const shreyakidney = users[0].kidneys;
-const numberofkid = shreyakidney.length;
-let  numberofheathofkid = 0;
-for(let i=0;i<shreyakidney.length;i++){
-    if(shreyakidney[i].heathly){
-        numberofheathofkid +=1 ;
+let users = [
+    {
+        name:"shreya",
+        kidneys:[
+            {
+                healthly:true
+            }
+        ]
     }
-}
-console.log(numberofheathofkid);
+]
 
-let unnumberofheathofkid = numberofkid - numberofheathofkid;
-console.log(unnumberofheathofkid);
+app.get("/",function(req,res){
+    let shreyalength = users[0].kidneys;
+    let numberofkidneys = shreyalength.length;
+    let numberofheakid = 0;
+    for( let i=0;i<shreyalength.length;i++)
+{
+    if(shreyalength[i].healthly){
+        numberofheakid +=1;
+    }
+
+    let numberofunheakid = numberofkidneys - numberofheakid;
+
+    res.json({
+        numberofkidneys,
+        numberofheakid,
+        numberofunheakid
+    })
+}})
+app.post("/",function(req,res){
+    let isheatly = req.body.isheatly;
+    users[0].kidneys.push({
+        healthly : isheatly
+    })
+    res.json({
+        msg:"done"
+    })
+    
+})
+app.put("/",function(req,res){
+    for(let i=0;i<user[0].kidneys.length;i++){
+        user[0].kidneys.healthly = true;
+    }
+
+    res.json({
+        
+    })
+
+    
+})
+
+app.listen(3001);
