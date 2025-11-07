04357059de6cde9f6c66efa31dc409b97a5393d1
shreyawagh343 <shreyawagh343@gmail.com>
2025-11-07 21:18:12 +0530

    day-7

---
+++ day-7/middleware.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+function middlewareFu(req,res,next){
+    let age = req.query.age;
+    if(age >= 18){
+        next();
+    }else {
+        res.status(401).json({
+            msg:"you are under age"
+        })
+    }
+}
+
+app.use(middlewareFu);
+app.get("/",function(req,res){
+    res.json({
+        msg: " you can ride"
+    })
+
+})
+app.get("/ride1",function(req,res){
+    res.json({
+        msg: " you can ride"
+    })
+
+})
+app.get("/ride2",function(req,res){
+    res.json({
+        msg: " you can ride"
+    })
+
+})
+
+app.listen(4000);
