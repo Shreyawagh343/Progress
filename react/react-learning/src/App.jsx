6ac57e1557bb988e5999755b4a098a2d58121beb
shreyawagh343 <shreyawagh343@gmail.com>
2025-11-21 23:07:39 +0530

    done

--- react/react-learning/src/App.jsx
+++ react/react-learning/src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react"
 import Topbar from "./Components/Topbar"
 
 function App() {
+  const [posts , setposts] = useState([{
+        username:"shreya",
+        password : "12345"
+        }])
+
+        function toggle(){
+          setposts([...posts , {
+            username:"raman",
+        password : "34523"
+          }])
+        }
+
+        const postman = posts.map((post)=><Topbar
+        username = {post.username}
+        password={post.password}        />)
 
   return (
     <>
-    <Topbar/>
-
+    <div style={{backgroundColor:"grey" , height:"10rem" , width:"30%", color:"white"}}>
+    <button onClick={toggle}>Add posts</button>
+    <div>{postman}</div>
+    </div>
     
+
     </>
   )
 }
--- react/react-learning/src/Components/Topbar.jsx
+++ react/react-learning/src/Components/Topbar.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
 
-function Topbar() {
-    const [ invisble , setinvisble] = useState(false);
+
+function Topbar({username , password}) {
+    // const [ invisble , setinvisble] = useState(false)
   return (
     <>
-{/* <div style={{backgroundColor:"grey" , height:"10rem" , width:"30%", color:"white"}}>
+    <div style={{backgroundColor:"grey" , height:"10rem" , width:"30%", color:"white"}}>
     
     <div>
-    {title !==undefined && <h1>{title}</h1>}
-            <p>28,000 follows</p>
+    {<h1>{username}</h1>}
+            <p>{password}</p>
             <p>12m</p>
         </div>
         <div>
             <p>gcihcgicbiewbfciewbufcbewuvbcfuveducvvuedcvuevdcdc</p>
         </div>
-        </div> */}
+        </div>
+
         
         {/* <button onClick={()=>{
             setinvisble(!invisble)
